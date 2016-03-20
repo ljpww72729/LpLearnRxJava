fdfcc721d3d7df5d44cf7510fb1afeef3b35ad52
ljpww72729 <ljpww72729@ljpww72729deMacBook-Pro.local>
2016-03-21 01:22:59 +0800

    Initial commit

---
+++ app/src/main/java/com/jww/lp/lplearnrxjava/MainActivity.java
@@ -0,0 +1,13 @@
+package com.jww.lp.lplearnrxjava;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
